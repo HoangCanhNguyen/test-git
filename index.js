c8208926911cc5618234a0906cd432fc8f8c2523
HoangCanhNguyen <canh.chn@gmail.com>
2020-11-05 16:08:26 +0700

    Create new project

---
+++ index.js
@@ -0,0 +1,18 @@
+var string_test = "hello world"
+var num = 9;
+
+var arr = [1,2,3,4,5];
+
+const checker = arr.every((x) => {
+  x < 40
+})
+
+const isBelowThreshold = (currentValue) => currentValue < 40;
+
+const array1 = [1, 30, 39, 29, 10, 13];
+
+console.log(array1.every(isBelowThreshold));
+
+console.log(checker);
+console.log(typeof(num));
+console.log(typeof(string_test  + num));
